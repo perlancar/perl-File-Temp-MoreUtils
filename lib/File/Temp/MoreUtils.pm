9ff16dbbd8d91def74ed516b2af818441d1236cb
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-14 14:31:28 +0700

    Add function: tempdir_named()

--- lib/File/Temp/MoreUtils.pm
+++ lib/File/Temp/MoreUtils.pm
@@ -14,7 +14,7 @@ use Fcntl ':DEFAULT';
 use File::Temp ();
 
 use Exporter 'import';
-our @EXPORT_OK = qw(tempfile_named);
+our @EXPORT_OK = qw(tempfile_named tempdir_named);
 
 our %SPEC;
 
@@ -71,19 +71,19 @@ _
     examples => [
         {
             args => {name=>'source.pdf'},
-            summary => 'Attempt to create source.pdf, then if already exists source.1.pdf, and so on',
+            summary => 'Attempt to create source.pdf, and if already exists source.1.pdf, and so on',
             test => 0,
             'x.doc.show_result' => 0,
         },
         {
             args => {name=>'source', dir=>undef},
-            summary => 'Attempt to create source.pdf in a temporary directory, then if already exists source.1, and so on',
+            summary => 'Attempt to create source.pdf in a temporary directory, and if already exists source.1, and so on',
             test => 0,
             'x.doc.show_result' => 0,
         },
         {
             args => {name=>'source.pdf', suffix_start=>'tmp1'},
-            summary => 'Attempt to create source.pdf, then if already exists source.tmp1.pdf, then source.tmp2.pdf, and so on',
+            summary => 'Attempt to create source.pdf, and if already exists source.tmp1.pdf, then source.tmp2.pdf, and so on',
             test => 0,
             'x.doc.show_result' => 0,
         },
@@ -128,6 +128,120 @@ sub tempfile_named {
         }
         if ($counter++ > 10_000) {
             die "tempfile_named(): Can't create temporary file after many retries: $!";
+        }
+    }
+}
+
+$SPEC{tempdir_named} = {
+    v => 1.1,
+    summary => 'Try to create a temporary directory with certain name '.
+        '(but used .1, .2, ... suffix if already exists) ',
+    description => <<'_',
+
+This is similar to `tempfile_named()`, but since there is no `O_EXCL` flag
+similar to opening a file, there is a race condition possible where you create a
+certain temporary directory and before you open/read the directory, someone else
+has replaced the directory with another. Therefore it is best if you create the
+specifically-named temporary directory _inside_ another temporary directory.
+
+Like <pm:File::Temp>'s `tempdir()`, it will return the path of the created
+temporary directory:
+
+    $dir
+
+_
+    result_naked => 1,
+    args => {
+        name => {
+            schema => 'filename*',
+            pos => 0,
+            req => 1,
+        },
+        dir => {
+            summary => 'If specified, will create the temporary directory here',
+            description => <<'_',
+
+If specified and set to `undef`, will create new temporary directory using
+<pm:File::Temp>'s `tempdir` (with CLEANUP option set to true unless DEBUG
+environment variable is set to true) and use this temporary directory for the
+directory, including for subsequent invocation for the same process whenever
+`dir` is set to `undef` again.
+
+_
+            schema => 'dirname',
+        },
+        suffix_start => {
+            schema => ['str*', min_len=>1],
+            default => 1,
+            description => <<'_',
+
+Will use Perl's post-increment operator (`++`) to increment the suffix, so this
+works with number as well as letter combinations, e.g. `aa` will be incremented
+to `ab`, `ac` and so on.
+
+_
+        },
+    },
+    examples => [
+        {
+            args => {name=>'/foo/source.dir'},
+            summary => 'Attempt to create /foo/source.dir/, and if already exists /foo/source.1.dir/ instead, and so on',
+            test => 0,
+            'x.doc.show_result' => 0,
+        },
+        {
+            args => {name=>'source', dir=>undef},
+            summary => 'Attempt to create source/ inside another temporary directory, and if already exists source.1/, and so on',
+            test => 0,
+            'x.doc.show_result' => 0,
+        },
+        {
+            args => {name=>'source.dir', suffix_start=>'tmp1'},
+            summary => 'Attempt to create source.dir/, and if already exists source.tmp1.dir/, then source.tmp2.pdf, and so on',
+            test => 0,
+            'x.doc.show_result' => 0,
+        },
+    ],
+};
+sub tempdir_named {
+    my %args = @_;
+
+    die "tempdir_named(): Please specify name" unless defined $args{name};
+
+    state $tempdir;
+    my $dir;
+    if (exists $args{dir}) {
+        if (defined $args{dir}) {
+            $dir = $args{dir};
+        } else {
+            $tempdir //= File::Temp::tempdir(CLEANUP => !$ENV{DEBUG});
+            $dir = $tempdir;
+        }
+        die "tempdir_named(): dir '$dir' is not a directory" unless -d $dir;
+    }
+
+    my $suffix_start = $args{suffix_start} // 1;
+    my $suffix;
+    my $fh;
+    my $name0 = defined $dir ? "$dir/" . File::Basename::basename($args{name}) : $args{name};
+    my $counter = 1;
+    while (1) {
+        my $name = $name0;
+        if (defined $suffix) {
+            $name =~ s/(.+\.)(?=.)/"$1$suffix."/e
+                or $name .= ".$suffix";
+            $suffix++;
+        } else {
+            $suffix = $suffix_start;
+        }
+        if (mkdir $name, 0700) {
+            return $name;
+        }
+        unless ($! == EEXIST) {
+            die "tempdir_named(): Can't create temporary dir '$name': $!";
+        }
+        if ($counter++ > 10_000) {
+            die "tempdir_named(): Can't create temporary dir after many retries: $!";
         }
     }
 }
--- t/01-basic.t
+++ t/01-basic.t
@@ -6,7 +6,7 @@ use Test::Exception;
 use Test::More 0.98;
 
 use File::Temp qw(tempdir);
-use File::Temp::MoreUtils qw(tempfile_named);
+use File::Temp::MoreUtils qw(tempfile_named tempdir_named);
 
 my $tempdir = tempdir(CLEANUP => !$ENV{DEBUG});
 subtest tempfile_named => sub {
@@ -20,6 +20,7 @@ subtest tempfile_named => sub {
 
     dies_ok { tempfile_named() } "no name arg -> dies";
     is_deeply([tempfile_named(name => "a")]->[1], "a.1");
+    ok(-f "a.1");
     is_deeply([tempfile_named(name => "a")]->[1], "a.2");
 
     is_deeply([tempfile_named(name => "b.txt")]->[1], "b.1.txt");
@@ -27,6 +28,8 @@ subtest tempfile_named => sub {
 
     is_deeply([tempfile_named(name => "c.")]->[1], "c..1");
     is_deeply([tempfile_named(name => "c.")]->[1], "c..2");
+
+    is_deeply([tempfile_named(name => "d1")]->[1], "d1.1");
 
     subtest "dir arg" => sub {
         is_deeply([tempfile_named(name => "a", dir=>"d1")]->[1], "d1/a");
@@ -41,6 +44,44 @@ subtest tempfile_named => sub {
     subtest "suffix_start arg" => sub {
         is_deeply([tempfile_named(name => "a", suffix_start=>"tmp1")]->[1], "a.tmp1");
         is_deeply([tempfile_named(name => "a", suffix_start=>"tmp1")]->[1], "a.tmp2");
+    };
+};
+
+subtest tempdir_named => sub {
+    chdir $tempdir or die;
+
+    my $fh;
+    open $fh, ">", "j" or die;
+    open $fh, ">", "k.dir" or die;
+    open $fh, ">", "l." or die;
+    mkdir "d2" or die;
+
+    dies_ok { tempdir_named() } "no name arg -> dies";
+    is_deeply(tempdir_named(name => "j"), "j.1");
+    ok(-d "j.1");
+    is_deeply(tempdir_named(name => "j"), "j.2");
+
+    is_deeply(tempdir_named(name => "k.dir"), "k.1.dir");
+    is_deeply(tempdir_named(name => "k.dir"), "k.2.dir");
+
+    is_deeply(tempdir_named(name => "l."), "l..1");
+    is_deeply(tempdir_named(name => "l."), "l..2");
+
+    is_deeply(tempdir_named(name => "d2"), "d2.1");
+
+    subtest "dir arg" => sub {
+        is_deeply(tempdir_named(name => "j", dir=>"d1"), "d1/j");
+        is_deeply(tempdir_named(name => "j", dir=>"d1"), "d1/j.1");
+
+        like(tempdir_named(name => "j", dir=>undef), qr{[/\\]j\z});
+        like(tempdir_named(name => "j", dir=>undef), qr{[/\\]j\.1\z});
+
+        dies_ok { tempdir_named(name => "j", dir=>"$tempdir/noexist") } "dir doesn't exist -> dies";
+    };
+
+    subtest "suffix_start arg" => sub {
+        is_deeply(tempdir_named(name => "j", suffix_start=>"tmp1"), "j.tmp1");
+        is_deeply(tempdir_named(name => "j", suffix_start=>"tmp1"), "j.tmp2");
     };
 };
 
